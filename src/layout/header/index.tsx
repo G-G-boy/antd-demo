7bf4a2aa297b42b43eace82ec626faf279bfab4a
Gavin.guo <Gavin.guo@dfox.cn>
2021-02-15 15:32:11 +0800

    fixed header

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -4,27 +4,57 @@ import SelectLang from '@/components/select-lang';
 import Account from '@/components/account';
 import Trigger from '@/components/trigger';
 import Setting from '@/components/setting';
+import {useSelector} from 'react-redux';
+import {ReducersType} from '@/store';
+import classNames from 'classnames';
 
 const {Header} = Layout;
 const {useBreakpoint} = Grid;
 
 const LayoutHeader: FC = () => {
     const {xs} = useBreakpoint();
+    const fixHeader = useSelector<ReducersType, boolean>((state) => state.setting.fixHeader);
+    const collapsed = useSelector<ReducersType, boolean>((state) => state.setting.collapsed);
+
+    const computedStyle = () => {
+        let styles;
+        if (fixHeader) {
+            if (collapsed) {
+                styles = {
+                    width: 'calc(100% - 80px)',
+                };
+            } else {
+                styles = {
+                    width: 'calc(100% - 200px)',
+                };
+            }
+        } else {
+            styles = {
+                width: '100%',
+            };
+        }
+        return styles;
+    };
+
+    const fixedClass = classNames({
+        fixed: fixHeader,
+        'top-0': fixHeader,
+        'right-0': fixHeader,
+    });
 
     return (
-        <Header className="bg-white px-4">
-            {xs && (
-                <div className="float-left h-full flex items-center">
-                    <Trigger />
-                </div>
-            )}
+        <>
+            {fixHeader ? <Header /> : null}
+            <Header style={computedStyle()} className={`bg-white px-4 ${fixedClass}`}>
+                <div className="float-left h-full flex items-center">{xs && <Trigger />}</div>
 
-            <div className="float-right h-full flex items-center">
-                <Setting />
-                <Account />
-                <SelectLang />
-            </div>
-        </Header>
+                <div className="float-right h-full flex items-center">
+                    <Setting />
+                    <Account />
+                    <SelectLang />
+                </div>
+            </Header>
+        </>
     );
 };
 
