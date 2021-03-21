858a8cf879fcce6885c466a55872055b0d967c10
Gavin.guo <Gavin.guo@dfox.cn>
2021-03-22 01:54:43 +0800

    路由切换动画

--- src/layout/content/index.tsx
+++ src/layout/content/index.tsx
@@ -1,9 +1,10 @@
 import {FC, lazy, Suspense} from 'react';
 import {Layout} from 'antd';
-import {Switch, Redirect} from 'react-router-dom';
+import {Switch, Redirect, useLocation} from 'react-router-dom';
 import Loading from '@/components/loading';
 import routeConfig, {RouteConfig} from '@/router/route-config';
 import SentryRoute from '@/router/SentryRoute';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 const {Content} = Layout;
 const NoFoundPage = lazy(() => import('@/pages/error/404'));
@@ -19,16 +20,21 @@ const routeMap = (routeConfig: RouteConfig) => {
 };
 
 const LayoutContent: FC = () => {
+    const location = useLocation();
     return (
         <Content style={{height: 'calc(100% - 64px)'}}>
-            <Suspense fallback={<Loading />}>
-                <Switch>
-                    <Redirect exact from="/" to="/dashboard" />
-                    {routeMap(routeConfig)}
-                    <SentryRoute path="/error/404" component={NoFoundPage} />
-                    <Redirect to="/error/404" />
-                </Switch>
-            </Suspense>
+            <TransitionGroup className="h-full">
+                <CSSTransition key={location.pathname} timeout={500} classNames="fade" exit={false}>
+                    <Suspense fallback={<Loading />}>
+                        <Switch location={location}>
+                            <Redirect exact from="/" to="/dashboard" />
+                            {routeMap(routeConfig)}
+                            <SentryRoute path="/error/404" component={NoFoundPage} />
+                            <Redirect to="/error/404" />
+                        </Switch>
+                    </Suspense>
+                </CSSTransition>
+            </TransitionGroup>
         </Content>
     );
 };
