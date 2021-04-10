9e5a2d926af6673681221f12376dcc3aa7a4eae8
Gavin.guo <Gavin.guo@dfox.cn>
2021-04-10 22:38:16 +0800

    更新依赖

--- config/webpack.prod.ts
+++ config/webpack.prod.ts
@@ -4,8 +4,8 @@ import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import TerserPlugin from 'terser-webpack-plugin';
 import CompressionPlugin from 'compression-webpack-plugin';
 import {webpackConfig} from './webpack.dev';
-import SentryWebpackPlugin from '@sentry/webpack-plugin';
-import pkg from '../package.json';
+// import SentryWebpackPlugin from '@sentry/webpack-plugin';
+// import pkg from '../package.json';
 
 const prodConfig = merge(webpackConfig, {
     mode: 'production',
@@ -18,17 +18,17 @@ const prodConfig = merge(webpackConfig, {
         }),
         new CompressionPlugin(),
         //NOTE:需要到node_module中执行sentry-cli的安装脚本
-        new SentryWebpackPlugin({
-            // sentry-cli configuration
-            authToken: 'd2c2f1f5994a401185de8abcfb0288979bd44eb7e6134f149c3bc2d27324ee6d',
-            org: 'sentry',
-            project: 'sentry-test',
-            release: `sentry-${pkg.version}`,
-
-            // webpack specific configuration
-            include: `${webpackConfig.output!.path}/js`,
-            ignore: ['node_modules'],
-        }),
+        // new SentryWebpackPlugin({
+        //     // sentry-cli configuration
+        //     authToken: 'd2c2f1f5994a401185de8abcfb0288979bd44eb7e6134f149c3bc2d27324ee6d',
+        //     org: 'sentry',
+        //     project: 'sentry-test',
+        //     release: `sentry-${pkg.version}`,
+        //
+        //     // webpack specific configuration
+        //     include: `${webpackConfig.output!.path}/js`,
+        //     ignore: ['node_modules'],
+        // }),
     ],
     optimization: {
         runtimeChunk: 'single',
